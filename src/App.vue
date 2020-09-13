a586a69cf66d9c6136f66d2fee0394f795c28256
ElafEssam <elafmuhammadessam@gmail.com>
2020-09-13 14:12:45 +0200

    last edit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <nav class="navbar navbar-expand-sm bg-light">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/Index" class="nav-link">Retrieve Item |</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/Create" class="nav-link">Create Item |</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/Edit" class="nav-link"> Edit Item </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 0;
+  width: 100%;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Create.vue
@@ -0,0 +1,46 @@
+[1:57 pm, 10/09/2020] Sahar: <template>
+
+ <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3>Add Item</h3>
+            </div>
+            <div class="card-body">
+                <form v-on:submit.prevent="addItem">
+                    <div class="form-group">
+                        <label>Item Name:</label>
+                        <input type="text" class="form-control" v-model="item.name"/>
+                    </div>
+                    <div class="form-group">
+                        <label>Item Price:</label>
+                        <input type="text" class="form-control" v-model="item.price"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Add Item"/>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  
+  data() {
+      return {
+          item: {}
+      }
+  },
+  methods: {
+      addItem() {
+           let uri = 'http://localhost:9945/item/';
+            this.axios.post(uri, this.item).then((response) => {
+                console.log(response.data.data)
+            });
+        }
+
+        
+    }
+}
+</script>
